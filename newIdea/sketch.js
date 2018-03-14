abe897dfec123cf0d91e2f21099d814a7f099759
Parikh2202334 <2202334@sps-student.sk.ca>
2018-03-14 15:18:13 -0600

    fixed
    fixed

--- newIdea/sketch.js
+++ newIdea/sketch.js
@@ -1,108 +1,57 @@
-// let hungryTommy; //the cursor image of tom cruise
-// let tomSnack; // the random image of money
-// let snackCoords = []; //the list used to store the location of the tomsnack for looping
-// let xCoordPullCode = 0;
-// let yCoordPullCode = 1;
-// let imgRangeX;
-// let imgRangeY;
-// let x = 0;
-// let snackX;
-// let snackY;
-// let snackXMax;
-// let snackYMax;
-//
-// function preload(){
-//   hungryTommy = loadImage("images/hungryTommy.png");
-//   tomSnack = loadImage("images/tomSnack.png");
-//
-// }
-//
-// function setup(){
-//   createCanvas(windowWidth,windowHeight);
-// }
-//
-// function draw(){
-//
-//
-//   let imgRangeX = mouseX;
-//   let imgRangeY = mouseY;
-//
-//   background(255);
-//
-//   imageMode(CENTER);
-//
-//   image(hungryTommy, mouseX, mouseY, 200, 200);
-//
-//   imageMode(CORNER);
-//
-//   image(tomSnack, snackCoords[xCoordPullCode], snackCoords[yCoordPullCode], 200, 200);
-//
-//   snackX = snackCoords[xCoordPullCode];
-//
-//   snackY = snackCoords[yCoordPullCode];
-//
-// }
-//
-// function mouseClicked() {
-//   if (mouseX >= snackX && mouseX <= snackX - 300 && mouseY >= snackY && mouseY <= snackY - 300){
-//     snackCoords.push(random(width - 200));
-//     snackCoords.push(random(height - 200));
-//     xCoordPullCode = xCoordPullCode + 1;
-//     yCoordPullCode = yCoordPullCode + 1;
-//     x += x;
-//   }
-//
-//
-// }
-// function keyTyped(){
-//   if (key === "a") {
-//     snackCoords.push(random(width - 200));
-//     snackCoords.push(random(height - 200));
-//     xCoordPullCode = xCoordPullCode + 1;
-//     yCoordPullCode = yCoordPullCode + 1;
-//     x += x;
-//   }
-// }
+let hungryTommy; //the cursor image of tom cruise
+let tomSnack; // the random image of money
+let snackCoords = []; //the list used to store the location of the tomsnack for looping
+let xCoordPullCode = 0; //the number used to reach into the array "snackCoords" and pull out the x value needed to loop the image in place
+let yCoordPullCode = 1; //the number used to reach into the array "snackCoords" and pull out the y value needed to loop the image in place
+let x = 0; //a variable i used to test whether or not my code was being executed how i wanted
+let snackX; //  the variable that is updated with the x value of the top left corner of the snack
+let snackY; // the variable that is updated with the y value of the top left corner of the snack
 
-let clockSize;
+function preload(){
+  hungryTommy = loadImage("images/hungryTommy.png"); //loads in the image of tom cruise that fills the cursor
+  tomSnack = loadImage("images/tomSnack.png"); //loads in the image of money that you must chase
+}
 
 function setup(){
   createCanvas(windowWidth,windowHeight);
-  if (width > height){
-    clockSize = height;
-  }
-  else {
-    clockSize = width;
-  }
-  angleMode(DEGREES);
 }
 
-function draw() {
+function draw(){
+
+
   background(255);
 
-  push();
-  translate(width / 2, height /2);
-  ellipse(0, 0, clockSize, clockSize);
+  imageMode(CENTER);
 
-  strokeWeight(10);
-  strokeCap(SQUARE);
+  image(hungryTommy, mouseX, mouseY, 200, 200);
 
-  for (let i = 0; i< 12; i++){
-    line(0, clockSize / 2 * 0.95, 0,clockSize/2 *0.9);
-    rotate(360/12);
+  imageMode(CORNER);
 
+  image(tomSnack, snackCoords[xCoordPullCode], snackCoords[yCoordPullCode], 200, 200);
+
+  snackX = snackCoords[xCoordPullCode];
+
+  snackY = snackCoords[yCoordPullCode];
+
+}
+
+function mouseClicked() {
+  if (mouseX >= snackX && mouseX <= snackX + 300 && mouseY >= snackY && mouseY <= snackY + 300){
+    snackCoords.push(random(width - 200));
+    snackCoords.push(random(height - 200));
+    xCoordPullCode = xCoordPullCode + 1;
+    yCoordPullCode = yCoordPullCode + 1;
+    x = 1 + x;
   }
 
-  strokeWeight(5);
 
-  for (let i = 0; i< 60; i++){
-    line(0, clockSize / 2 * 0.7, 0,clockSize/2 *0.9);
-    rotate(360/60);
-
+}
+function keyTyped(){
+  if (key === "a") {
+    snackCoords.push(random(width - 200));
+    snackCoords.push(random(height - 200));
+    xCoordPullCode = xCoordPullCode + 1;
+    yCoordPullCode = yCoordPullCode + 1;
+    x = x + 1;
   }
-
-  line(0, clockSize / 2 * 0.95, 0,clockSize/2 *0.9);
-
-  pop();
-
 }
