4ad7decb4682b1e1915b6a0fbd9cbf61a5dce5d0
Jay Parikh <jayparikh37@gmail.com>
2018-03-14 13:57:47 -0600

    some fixes, still doesnt work
    still doesnt work

--- newIdea/sketch.js
+++ newIdea/sketch.js
@@ -1,9 +1,15 @@
 let hungryTommy; //the cursor image of tom cruise
 let tomSnack; // the random image of money
 let snackCoords = []; //the list used to store the location of the tomsnack for looping
-let snackGenerate = true;
 let xCoordPullCode = 0;
 let yCoordPullCode = 1;
+let imgRangeX;
+let imgRangeY;
+let x = 0;
+let snackX;
+let snackY;
+let snackXMax;
+let snackYMax;
 
 function preload(){
   hungryTommy = loadImage("images/hungryTommy.png");
@@ -18,19 +24,42 @@ function setup(){
 function draw(){
 
 
-  let imgRangeX = mouseX -100;
-  let imgRangeY = mouseY -100;
+  let imgRangeX = mouseX;
+  let imgRangeY = mouseY;
 
   background(255);
 
-  image(hungryTommy, mouseX - 100, mouseY - 100, 200, 200);
+  imageMode(CENTER);
+
+  image(hungryTommy, mouseX, mouseY, 200, 200);
+
+  imageMode(CORNER);
+
   image(tomSnack, snackCoords[xCoordPullCode], snackCoords[yCoordPullCode], 200, 200);
 
-}
-function mouseClicked() {
-  snackCoords.push(random(200, width - 200));
-  snackCoords.push(random(200, height - 200));
-  xCoordPullCode = xCoordPullCode + 1;
-  yCoordPullCode = yCoordPullCode + 1;
+  snackX = snackCoords[xCoordPullCode];
 
+  snackY = snackCoords[yCoordPullCode];
+
+}
+
+function mouseClicked() {
+  if (mouseX >= snackX && mouseX <= snackX - 300 && mouseY >= snackY && mouseY <= snackY - 300){
+    snackCoords.push(random(width - 200));
+    snackCoords.push(random(height - 200));
+    xCoordPullCode = xCoordPullCode + 1;
+    yCoordPullCode = yCoordPullCode + 1;
+    x += x;
+  }
+
+
+}
+function keyTyped(){
+  if (key === "a") {
+    snackCoords.push(random(width - 200));
+    snackCoords.push(random(height - 200));
+    xCoordPullCode = xCoordPullCode + 1;
+    yCoordPullCode = yCoordPullCode + 1;
+    x += x;
+  }
 }
